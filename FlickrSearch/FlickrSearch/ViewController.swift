92b558ac95947e500a23048b8852599e43d3438b
setsu <zizi4swift@gmail.com>
2020-02-15 10:39:44 +0900

    プロジェクトを作成する

---
+++ FlickrSearch/FlickrSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrSearch
+//
+//  Created by setsu on 2020/02/15.
+//  Copyright © 2020 setsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
